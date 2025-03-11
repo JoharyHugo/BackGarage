5041e9288c9971710ff785c66667a390873085a8
ChristelRav <ravmihary@gmail.com>
2025-03-11 10:35:34 +0300

    init modele utilisateur + profil
    -hachage mdp
    -profil(client-mécanicien-manager)

---
+++ models/md_profil.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const ProfilSchema = new mongoose.Schema({
+  nomprofil: { type: String, required: true, unique: true }
+});
+
+
+module.exports = mongoose.model('Profil', ProfilSchema);
