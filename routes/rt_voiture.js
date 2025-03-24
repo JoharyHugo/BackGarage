13f5047de480564434ca828747515a321c79aec4
RAMAROSANDRATANA johary ny aina nirina <joharyny@gmail.com>
2025-03-25 00:20:36 +0100

    Update rt_voiture.js

--- routes/rt_voiture.js
+++ routes/rt_voiture.js
@@ -74,7 +74,10 @@ router.post('/ajouterVoiture', protect, async (req, res) => {
 // lister les voitures pour un client
 router.get('/listVoiturebyclient', protect, async (req, res) => {
   try {
-  const voitures = await Voiture.find({ idclient: req.user.userId });
+  // const voitures = await Voiture.find({ idclient: req.user.userId });
+  const voitures = await Voiture.find({ idclient: req.user.userId })
+  .populate('idmarque') // Remplir complètement l'objet "Marque"
+  .populate('idcategorie'); // Remplir complètement l'objet "Categorie"
   console.log("ID =====",req.user.userId );
   if (voitures.length === 0) {
     return res.status(400).json({ message: 'Aucune voiture trouvée pour cet utilisateur.' });
